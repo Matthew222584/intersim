60929b9bbb62acede6cbfebe46bc6a1c8d9de0c5
Ubuntu <ubuntu@ip-172-31-21-188.us-east-2.compute.internal>
2024-03-23 21:52:11 +0000

    debugging - route not found

--- backend2/routing/urls.py
+++ backend2/routing/urls.py
@@ -19,5 +19,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sentiment/', views.sentiment, name='sentimentAnalysis'),
+    path('sentiment/', views.sentiment, name='sentiment'),
 ]
