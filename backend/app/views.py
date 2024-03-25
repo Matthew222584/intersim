535220c1bc8d45e8fad17a022c81cfec07222fca
Eric-Jiang1 <ericjiang0325@gmail.com>
2024-03-25 13:03:14 -0400

    s

--- backend/app/views.py
+++ backend/app/views.py
@@ -91,15 +91,8 @@ def getquestions(request):
     if request.method != 'GET':
         return HttpResponse(status=404)
 
-    try:
-        json_data = json.loads(request.body)
-        username = json_data.get('username')
-        num_questions = json_data.get('num_questions')
-    except json.JSONDecodeError:
-        return JsonResponse({'error': 'Invalid JSON format'}, status=400)
-
-    # username = request.GET.get('username')
-    # num_questions = request.GET.get('num_questions')
+    username = request.GET.get('username')
+    num_questions = request.GET.get('num_questions')
 
     # make sure username and questions are passed in
     if not username or not num_questions:
