577ae20d57833905644735f04290179ee2f60170
Eric-Jiang1 <ericjiang0325@gmail.com>
2024-03-25 14:27:27 -0400

    s

--- backend/app/views.py
+++ backend/app/views.py
@@ -219,6 +219,7 @@ def getfeedback(request):
 
     username = request.GET.get('username')
     interview_id = request.GET.get('interview_id')
+    return JsonResponse({"username": username, "interview_id": interview_id}, safe=False, status=200)
 
     query = """
         SELECT e.question, e.emotion, e.accuracy, qr.question_answer
@@ -228,7 +229,6 @@ def getfeedback(request):
         AND e.interview_id = %s
         ORDER BY e.question_id ASC, e.emotion ASC;
     """
-
     # TO DO: query to get speech emotion analysis
 
     # Execute the SQL query
